ccbd6d606a0d30c62eed50bc418251413e6b4ecd
RicardoVessaro <ricardolvessaro@gmail.com>
2023-09-30 11:57:05 -0300

    Extending the UserDetails

--- spring/jwt/src/main/java/com/alibou/security/user/User.java
+++ spring/jwt/src/main/java/com/alibou/security/user/User.java
@@ -5,6 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -17,8 +23,13 @@ import lombok.NoArgsConstructor;
  *  created in PostgresSQl for example, maybe in MariaDB
  *  also. Our user table name is '_user'.
  */
+/*
+ * UserDetails is used by Spring to handle security
+ *  functionalities. Implement it do customizer the
+ *  User according to the business logic.
+ */
 @Table(name="_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue // Default values is GenerationType.AUTO.
@@ -27,5 +38,40 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
