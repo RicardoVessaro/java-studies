7760a9e1126a23b70994544c8832a4690b80b20c
RicardoVessaro <ricardolvessaro@gmail.com>
2023-09-30 12:28:40 -0300

    Checking the JWT token

--- spring/jwt/src/main/java/com/alibou/security/config/JwtAuthenticationFilter.java
+++ spring/jwt/src/main/java/com/alibou/security/config/JwtAuthenticationFilter.java
@@ -19,13 +19,33 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
-    // @NonNull: These parameters should not be null.
+    /*
+     * @NonNull: These parameters should not be null.
+     */
     @Override
     protected void doFilterInternal(
         @NonNull HttpServletRequest request, // To access information about the request.
         @NonNull HttpServletResponse response, // To access and add information in the response.
         @NonNull FilterChain filterChain // Follows the filter chain pattern, it is used to call the next filter.
     ) throws ServletException, IOException {
+        /*
+         *  When we make a call we need to pass the JWT authentication token within the header.
+         *  request.getHeader("Authorization"): Gets the Authorization header.
+         */
+        final String authHeader = request.getHeader("Authorization");
 
+        final String jwt;
+        /*
+         * If there is no auth header or "Bearer " on in then do nothing.
+         */
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response); // Calling the next filter.
+            return;
+        }
+
+        /*
+         * Begins in 7 because "Bearear " has 8 characters
+         */
+        jwt = authHeader.substring(7);
     }
 }
