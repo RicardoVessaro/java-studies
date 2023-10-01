e0915a324ffa41f6445b3b3e2226ee719c307021
RicardoVessaro <ricardolvessaro@gmail.com>
2023-10-01 14:51:56 -0300

    Creating the logout handler service

--- spring/jwt/src/main/java/com/alibou/security/config/SecurityConfiguration.java
+++ spring/jwt/src/main/java/com/alibou/security/config/SecurityConfiguration.java
@@ -8,8 +8,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +20,7 @@ public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -58,7 +61,24 @@ public class SecurityConfiguration {
              * Calling our filter before the UsernamePasswordAuthenticationFilter.
              * Because we authenticate the user in the jwtAuthFilter.
              */
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .logout(logout -> {
+                /*
+                 * In Spring, we don't need to provide a logout endpoint, when this url
+                 *  is requested Spring will call the LogoutHandler.
+                 */
+                logout.logoutUrl("/api/v1/auth/logout");
+                /*
+                 * This is where we implement the logout mechanism.
+                 */
+                logout.addLogoutHandler(logoutHandler);
+                /*
+                 * Once the user logout we need to clear the context in order to the user have not access to the api.
+                 */
+                logout.logoutSuccessHandler(((request, response, authentication) ->
+                    SecurityContextHolder.clearContext()));
+            })
+            ;
 
 
         return http.build();
