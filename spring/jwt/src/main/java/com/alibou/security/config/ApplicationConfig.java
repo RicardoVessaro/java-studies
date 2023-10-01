ff15652d1a819193902155548a34b5d907a9ea98
RicardoVessaro <ricardolvessaro@gmail.com>
2023-09-30 23:52:13 -0300

    Creating the authentication provider bean

--- spring/jwt/src/main/java/com/alibou/security/config/ApplicationConfig.java
+++ spring/jwt/src/main/java/com/alibou/security/config/ApplicationConfig.java
@@ -4,9 +4,13 @@ import com.alibou.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,6 +23,33 @@ public class ApplicationConfig {
         // The implemenation of UserDetailsService can be replaced by a lambda expression.
         return username -> repository.findByEmail(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    /*
+     * The authentication provider is the data acces object (DAO)
+     *  which is responsible to fecth the user details and also
+     *  encode password and so on so forth so for this we use the
+     *  DaoAuthenticationProvider (but there are other implementations
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        /**
+         * Tell the provider which service it should use.
+         */
+        authProvider.setUserDetailsService(userDetailsService());
+
+        /*
+         * Also needs to pass which password encoder we are using.
+         */
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
