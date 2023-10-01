5e4779510c828286f894eb52d3c4d7276bac05bb
RicardoVessaro <ricardolvessaro@gmail.com>
2023-09-30 23:41:41 -0300

    Adding the security configuration - deprecated

---
+++ spring/jwt/src/main/java/com/alibou/security/config/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.alibou.security.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+            /*
+             * Disabling CSRF (We will talk about it in a different video) [Search for the video].
+             */
+            .csrf()
+            .disable()
+            /**
+             * Whitelist endpoints are endpoints that don't require authentication like login and create account.
+             * Authorizing any request in the Matchers
+             */
+            .authorizeHttpRequests()
+            .requestMatchers("")
+            .permitAll()
+            /*
+             * Any other request should be authenticated.
+             */
+            .anyRequest()
+            .authenticated()
+            .and() /* And indicates new configuration */
+            .sessionManagement()
+            /*
+             * How we want to create our session.
+             * In this case stateless so every request is authenticated.
+             */
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authenticationProvider(authenticationProvider)
+            /*
+             * Calling our filter before the UsernamePasswordAuthenticationFilter.
+             * Because we authenticate the user in the jwtAuthFilter.
+             */
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+
+}
